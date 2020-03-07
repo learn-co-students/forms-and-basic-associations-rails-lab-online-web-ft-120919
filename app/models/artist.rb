67c88dc5abd5a4f2eb13c6d9e0c9a5a875b532bf
ricardo39985 <ricardo39985@gmail.com>
2020-03-07 09:16:55 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,20 @@
 class Artist < ActiveRecord::Base
   # add associations here
+  has_many :songs
 end
+=begin
+Artist
+has a name attribute (String)
+has many Songs
+Song
+has a title attribute (String)
+belongs to an Artist
+belongs to a Genre
+has many Notes
+Genre
+has a name attribute (String)
+has many songs
+Note
+has content attribute (String)
+belongs to a Song
+=end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,36 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+  accepts_nested_attributes_for :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)    
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_content(array)
+    array.each do |note|
+      if !note.empty?
+        self.notes.build(content: note)
+      end
+    end
+  end
+
+  def note_content
+    self.notes.map do |note|
+      note.content
+    end   
+  end
 end
+=begin
+Song
+has a title attribute (String)
+belongs to an Artist
+belongs to a Genre
+has many Notes
+=end
